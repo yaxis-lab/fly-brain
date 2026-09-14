adbba2cc5b3ae8acb133ed0ac3b423d81ec04d7e
SaM-0777 <sam.2.3.4.5.ranjan@gmail.com>
2026-09-14 18:39:05 +0530

    fix(ui): removed unused import and ui component

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { Sidebar } from "@/components/layout/Sidebar";
 import { BrainViewer } from "@/components/viewer/BrainViewer";
-import { NeuroglancerTest } from "@/components/neuroglancer/NeuroglancerTest";
 
 export default function Home() {
   return (
@@ -12,7 +11,6 @@ export default function Home() {
       <main className="relative min-w-0 flex-1 h-full w-full ms-28">
         <BrainViewer />
       </main>
-      {/*<NeuroglancerTest />*/}
     </div>
   );
 }
