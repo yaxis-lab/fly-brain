37e7c50cf2551d76b5887f66b5d610edc1ed9059
SaM-0777 <sam.2.3.4.5.ranjan@gmail.com>
2026-09-16 15:40:17 +0530

    feat(brain2): experiments

---
+++ experiments/Neuron.py
@@ -0,0 +1,36 @@
+from brian2 import *
+
+start_scope()
+
+N = 1000
+tau = 10*ms
+vr = -70*mV
+vt0 = -50*mV
+delta_vt0 = 5*mV
+tau_t = 100*ms
+sigma = 0.5*(vt0-vr)
+v_drive = 2*(vt0-vr)
+duration = 100*ms
+
+eqs = '''
+dv/dt = (v_drive+vr-v)/tau + sigma*xi*tau**-0.5 : volt
+dvt/dt = (vt0-vt)/tau_t : volt
+'''
+
+reset = '''
+v = vr
+vt += delta_vt0
+'''
+
+G = NeuronGroup(N, eqs, threshold='v>vt', reset=reset, refractory=5*ms, method='euler')
+spikemon = SpikeMonitor(G)
+
+G.v = 'rand()*(vt0-vr)+vr'
+G.vt = vt0
+
+run(duration)
+
+_ = hist(spikemon.t/ms, 100, histtype='stepfilled', facecolor='k', weights=list(ones(len(spikemon))/(N*defaultclock.dt)))
+xlabel('Time (ms)')
+ylabel('Instantaneous firing rate (sp/s)');
+show()
---
+++ experiments/network_operation_stop.py
@@ -0,0 +1,29 @@
+from brian2 import *
+
+N = 10
+tau = 10 * ms
+I = 1.5
+target_spikes = 20
+
+G = NeuronGroup(
+    N, "dv/dt = (I - v)/tau : 1", threshold="v>1", reset="v=0", method="exact"
+)
+G.v = "rand()"
+
+spike_mon = SpikeMonitor(G)
+
+
+@network_operation(dt=1 * ms)
+def check_spikes():
+    if spike_mon.num_spikes >= target_spikes:
+        print(f"Stopped at t={defaultclock.t} with {spike_mon.num_spikes} spikes")
+        stop()
+
+
+run(100 * ms)
+
+plot(spike_mon.t / ms, spike_mon.i, ".k")
+xlabel("Time (ms)")
+ylabel("Neuron index")
+title("Spike raster plot")
+show()
